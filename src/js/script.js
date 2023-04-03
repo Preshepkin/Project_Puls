3d1011683b5a37e77037b35de1a3504c9b4c1f40
Kirill <Preshepkin73@gmail.com>
2023-04-03 12:20:48 +0300

    add/03.04.23

--- src/js/script.js
+++ src/js/script.js
@@ -1,22 +1,26 @@
 
-const slider = tns({
-	container: '.carousel__inner',
-	items: 1,
-	slideBy: 'page',
-	speed:	1800,
-	controls:false,
-	autoplay: false,
-	nav:false
-  });
+// const slider = tns({
+// 	container: '.carousel__inner',
+// 	items: 1,
+// 	slideBy: 'page',
+// 	speed:	1800,
+// 	controls:false,
+// 	autoplay: false,
+// 	nav:false
+//   });
 
-  document.querySelector('.prev').addEventListener('click',function	()	
-  {slider.goTo('prev')});
-
-  document.querySelector('.next').addEventListener('click',function	()	
-  {slider.goTo('next')});   
-
-  $('ul.catalog__tabs').on('click', 'li:not(.active)', function() {
-    $(this)
-      .addClass('active').siblings().removeClass('active')
-      .closest('div.tabs').find('div.catalog__tab').removeClass('active').eq($(this).index()).addClass('active');
+$(document).ready(function(){
+	$('.carousel__inner').slick({
+		dots: false,
+		infinite: true,
+		speed: 1600,
+		slidesToShow: 1,
+		prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
+		nextArrow:'<button type="button" class="slick-next"><img src="../icons/right.svg"></button>'
+	  });
+	  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+		$(this)
+		  .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+		  .closest('div.container').find('catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+	  });	  
   });
