0e2b777cb995e39c6ee57c49bf3504956b4b97ab
Kirill <Preshepkin73@gmail.com>
2023-04-18 15:05:28 +0300

    add/media

--- src/js/script.js
+++ src/js/script.js
@@ -6,7 +6,27 @@ $(document).ready(function(){
 		speed: 1600,
 		slidesToShow: 1,
 		prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
-		nextArrow:'<button type="button" class="slick-next"><img src="../icons/right.svg"></button>'
+		nextArrow:'<button type="button" class="slick-next"><img src="../icons/right.svg"></button>',
+		responsive: [
+			{
+			  breakpoint: 991,
+			  settings: {
+				arrows: false,
+				centerMode: true,
+				centerPadding: '40px',
+				slidesToShow: 1
+			  }
+			},
+			{
+			  breakpoint: 767,
+			  settings: {
+				arrows: false,
+				centerMode: true,
+				centerPadding: '40px',
+				slidesToShow: 1
+			  }
+			}
+		  ],
 	  });
 
 
